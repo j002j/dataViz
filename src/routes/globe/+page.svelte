64334d9f09f9ba5979ed8e4c0e17c1cff442f3f2
Jennifer Janac <jennifer.janac@study.hs-duesseldorf.de>
2026-02-10 14:59:37 +0100

    svelte setup added

---
+++ src/routes/globe/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    import CosmosCanvas from "$lib/visualization/CosmosCanvas.svelte";
+</script>
+
+<CosmosCanvas />
+<section class="flex-grow bg-orange-500 relative min-h-[500px]"></section>
